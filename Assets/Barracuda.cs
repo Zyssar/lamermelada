78e0e4a1d2ed6aeaf581e0dcb2730ca024b3f388
Chavitops <javierchurubio@gmail.com>
2025-01-25 20:39:26 -0600

    aadsdsa

--- Assets/Barracuda.cs
+++ Assets/Barracuda.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Barracuda : MonoBehaviour
+public class Barracuda : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public Transform playerPos;
+    [SerializeField] public new int difficulty = 4;
+    public void Start()
     {
-        
     }
 
     // Update is called once per frame
--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public int difficulty = 0;
+    public int IA = 1;
     public bool active = true;
+    public Enemy[] enemies;
+    private int roll;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        IA = Random.Range(1, 10);
+        Debug.Log("IA: " +  IA.ToString());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(Spawn());
     }
 
     IEnumerator Spawn()
     {
-        yield return new WaitForSeconds(5/difficulty);
+        yield return new WaitForSeconds(30/IA);
+        roll = Random.Range(0, 10);
+        Debug.Log(roll.ToString());
+        if (IA + roll >=15)
+        {
+            foreach (Enemy enemy in enemies)
+            {
+                if (enemy.difficulty == 4)
+                    Instantiate(enemy, transform.position, Quaternion.identity);
+            }
+        }
+        else
+        {
+            Debug.Log("XD");
+        }
     }
 }
